a9be83fffd28a23fdc2a5282bcb1a62451989eb3
Alexander <sancho_8484@mail.ru>
2023-09-16 21:02:44 +0700

    added tasks from homework

---
+++ 9/задача_66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumDigits(int M, int N)
+{
+    if (M > N) return 0;
+    return M + SumDigits(++M, N);
+}
+
+
+int numFirst = InputNum("Введите первое число: ");
+int numSecond = InputNum("Введите второе число: ");
+int sum = SumDigits(numFirst, numSecond);
+System.Console.WriteLine(sum);
---
+++ 9/задача_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Ackermann(int m, int n)
+{
+    while (m != 0)
+    {
+
+        if (n == 0) n = 1;
+        else n = Ackermann(m, n - 1);
+             
+        m = m - 1;  
+    }
+    return n + 1;
+}
+
+int m = InputNum("Введите первое число: ");
+int n = InputNum("Введите второе число: ");
+System.Console.WriteLine($"A({m}, {n}) = {Ackermann(m, n)}");
